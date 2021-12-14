3c90751b5028929b06f305932a67e81e20e6703b
Niklas Kuder <nkqdr@users.noreply.github.com>
2021-12-14 14:00:49 +0100

    Implemented geolocation

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,24 @@
 function testButton() {
   console.log("Clicked.");
 }
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition, showError);
+  } else {
+    document.getElementById("page-content").innerHTML =
+      "Geolocation is not supported by this browser.";
+  }
+}
+
+function showError(error) {
+  console.log(error);
+}
+
+function showPosition(position) {
+  document.getElementById("page-content").innerHTML =
+    "Latitude: " +
+    position.coords.latitude +
+    "<br>Longitude: " +
+    position.coords.longitude;
+}
