960b64b567fcb5ce274cad2ca7cde5ca7a59ec9c
Niklas Kuder <nkqdr@users.noreply.github.com>
2021-12-15 15:48:23 +0100

    changed deviceorientation handler

--- src/scripts/scale.js
+++ src/scripts/scale.js
@@ -41,39 +41,41 @@ var vx = 0.0; // Velocity x and y
 var vy = 0.0;
 var updateRate = 1 / 60; // Sensor refresh rate
 
+function orientationListener(event) {
+  // Expose each orientation angle in a more readable way
+  rotation_degrees = event.alpha;
+  frontToBack_degrees = event.beta;
+  leftToRight_degrees = event.gamma;
+
+  // Update velocity according to how tilted the phone is
+  // Since phones are narrower than they are long, double the increase to the x velocity
+  vx = vx + leftToRight_degrees * updateRate * 2;
+  vy = vy + frontToBack_degrees * updateRate;
+
+  // Update position and clip it to bounds
+  px = px + vx * 0.5;
+  if (px > 98 || px < 0) {
+    px = Math.max(0, Math.min(98, px)); // Clip px between 0-98
+    vx = 0;
+  }
+
+  py = py + vy * 0.5;
+  if (py > 98 || py < 0) {
+    py = Math.max(0, Math.min(98, py)); // Clip py between 0-98
+    vy = 0;
+  }
+
+  dot = document.getElementsByClassName("indicatorDot")[0];
+  dot.setAttribute("style", "left:" + px + "%;" + "top:" + py + "%;");
+}
+
 function getAccel() {
   if (isMobile.iOS()) {
     DeviceMotionEvent.requestPermission().then((response) => {
       if (response == "granted") {
         // Add a listener to get smartphone orientation
         // in the alpha-beta-gamma axes (units in degrees)
-        window.addEventListener("deviceorientation", (event) => {
-          // Expose each orientation angle in a more readable way
-          rotation_degrees = event.alpha;
-          frontToBack_degrees = event.beta;
-          leftToRight_degrees = event.gamma;
-
-          // Update velocity according to how tilted the phone is
-          // Since phones are narrower than they are long, double the increase to the x velocity
-          vx = vx + leftToRight_degrees * updateRate * 2;
-          vy = vy + frontToBack_degrees * updateRate;
-
-          // Update position and clip it to bounds
-          px = px + vx * 0.5;
-          if (px > 98 || px < 0) {
-            px = Math.max(0, Math.min(98, px)); // Clip px between 0-98
-            vx = 0;
-          }
-
-          py = py + vy * 0.5;
-          if (py > 98 || py < 0) {
-            py = Math.max(0, Math.min(98, py)); // Clip py between 0-98
-            vy = 0;
-          }
-
-          dot = document.getElementsByClassName("indicatorDot")[0];
-          dot.setAttribute("style", "left:" + px + "%;" + "top:" + py + "%;");
-        });
+        window.addEventListener("deviceorientation", orientationListener);
       }
     });
   } else {
@@ -81,6 +83,7 @@ function getAccel() {
       alert("no accelerometer");
     } else {
       alert("accelerometer found");
+      window.addEventListener("deviceorientation", orientationListener);
     }
   }
 }
